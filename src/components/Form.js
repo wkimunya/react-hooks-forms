ebbaeedfedec389a769597e80dff60baa3d09aed
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-08-29 22:53:54 +0300

    complete assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,13 +4,35 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+   
+    console.log("Form submitted with:", firstName, lastName);
+  };
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={firstName}
+        onChange={handleFirstNameChange} // Handle first name input change
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={handleLastNameChange} // Handle last name input change
+      />
       <button type="submit">Submit</button>
     </form>
   );
 }
+
 
 export default Form;
